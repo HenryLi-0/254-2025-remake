49bc471f7538b68a6f71dfaf63ab91f03f5cf7d8
HenryLi-0 <bxsci8266@gmail.com>
2025-09-02 17:52:18 -0400

    untested code for the shooter's hood

--- src/main/java/org/sciborgs1155/robot/shooter/Shooter.java
+++ src/main/java/org/sciborgs1155/robot/shooter/Shooter.java
@@ -9,11 +9,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.sciborgs1155.lib.LoggingUtils;
 import org.sciborgs1155.robot.Robot;
 import org.sciborgs1155.robot.shooter.ShooterConstants.Flywheel;
+import org.sciborgs1155.robot.shooter.ShooterConstants.Hood;
 
 public class Shooter extends SubsystemBase {
   private final ShooterIO hardware;
   private final PIDController flywheelPIDController;
   private final SimpleMotorFeedforward flywheelFF;
+  private final PIDController hoodPIDController;
+  private final SimpleMotorFeedforward hoodFF;
 
   public static Shooter create() {
     return Robot.isReal() ? new Shooter(new RealShooter()) : new Shooter(new SimShooter());
@@ -27,17 +30,27 @@ public class Shooter extends SubsystemBase {
     this.hardware = hardware;
     flywheelPIDController = new PIDController(Flywheel.kP, Flywheel.kI, Flywheel.kD);
     flywheelFF = new SimpleMotorFeedforward(Flywheel.kS, Flywheel.kV, Flywheel.kA);
+
+    hoodPIDController = new PIDController(Hood.kP, Hood.kI, Hood.kD);
+    hoodFF = new SimpleMotorFeedforward(Hood.kS, Hood.kV, Hood.kA);
   }
 
   /**
    * Sets the flywheel goal velocity.
    *
    * @param goal The target velocity.
-   * @return
    */
   public Command setFlywheelGoal(double goal) {
-    return Commands.sequence(
-        Commands.runOnce(() -> setFlywheelSetpoint(goal)), Commands.run(() -> flywheelUpdate()));
+    return Commands.runOnce(() -> setFlywheelSetpoint(goal));
+  }
+
+  /**
+   * Sets the hood goal position.
+   *
+   * @param goal The target position.
+   */
+  public Command setHoodGoal(double goal) {
+    return Commands.runOnce(() -> setHoodSetpoint(goal));
   }
 
   /** Sets the flywheel setpoint velocity. */
@@ -54,9 +67,26 @@ public class Shooter extends SubsystemBase {
     LoggingUtils.log("flywheel velocity actual", hardware.flywheelVelocity());
   }
 
+  /** Sets the hood setpoint position. */
+  private void setHoodSetpoint(double goal) {
+    hoodPIDController.setSetpoint(goal);
+  }
+
+  /** Updates the hood feedback and feedforward. */
+  private void hoodUpdate() {
+    double fb = hoodPIDController.calculate(hardware.hoodPosition());
+    double ff = hoodFF.calculate(hoodPIDController.getSetpoint());
+    hardware.setHoodVoltage(MathUtil.clamp(fb + ff, -12, 12));
+    LoggingUtils.log("hood voltage", MathUtil.clamp(fb + ff, -12, 12));
+    LoggingUtils.log("hood position actual", hardware.hoodPosition());
+  }
+
+    
   @Override
   public void periodic() {
     flywheelUpdate();
+    hoodUpdate();
     LoggingUtils.log("flywheel velocity setpoint", flywheelPIDController.getSetpoint());
+    LoggingUtils.log("hood position setpoint", hoodPIDController.getSetpoint());
   }
 }
